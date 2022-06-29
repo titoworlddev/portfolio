db7aff04d55a8eae4cbed64a256374724593bada
Hekhy <titocristian.dev@gmail.com>
2022-06-29 20:02:12 +0200

    Fized the problems with the routes when you open the web from the index.html and not from live server

--- js/main.js
+++ js/main.js
@@ -50,35 +50,35 @@
         tarjeta.querySelector('#texto-tarjeta').innerHTML =
           'Random X es tu ayudante a la hora de jugar y/o divertirte con amigos, sus generadores aleatorios te brindan opciones para elegir de forma aleatoria quien paga la cena, hacer el amigo invisible con amigos o familiares, hacer un bingo, jugar a tu propio Scattergories o lo que se te ocurra. Sin duda el compañero perfecto en estos casos, no esperes más y que empiece la diversión.';
         tarjeta.querySelector('.fotos-tarjeta img').src =
-          '/assets/img/trabajos/random_x/randomx_presentation.png';
+          '../assets/img/trabajos/random_x/randomx_presentation.png';
         break;
       case 'Operit':
         tarjeta.querySelector('#titulo-tarjeta').innerHTML = 'Operit';
         tarjeta.querySelector('#texto-tarjeta').innerHTML =
           'Calculadora con diseño simple pero muy completa en sus funciones.Si buscas hacer operaciones rápidas tienes su parte básica, si por el contrario eres un usuario más avanzado puedes usar la calculadora científica.También hay varios ajustes para personalizar la app a tu gusto y una página de ayuda por si no comprendes alguna de las funciones.¡Sin duda una calculadora que será fácil y divertida de usar, disfrútala!';
         tarjeta.querySelector('.fotos-tarjeta img').src =
-          '/assets/img/trabajos/random_x/randomx_presentation.png';
+          '../assets/img/trabajos/random_x/randomx_presentation.png';
         break;
       case 'Game Zone':
         tarjeta.querySelector('#titulo-tarjeta').innerHTML = 'Game Zone';
         tarjeta.querySelector('#texto-tarjeta').innerHTML =
           'Continuando con los diseños aqui dejo uno nuevo, en este caso de una aplicacion de movil sobre una tienda de videojuegos, que tambien podria servir como base para una tienda en general. Espero que os guste.';
         tarjeta.querySelector('.fotos-tarjeta img').src =
-          '/assets/img/trabajos/random_x/randomx_presentation.png';
+          '../assets/img/trabajos/random_x/randomx_presentation.png';
         break;
       case 'El Chingón':
         tarjeta.querySelector('#titulo-tarjeta').innerHTML = 'El Chingón';
         tarjeta.querySelector('#texto-tarjeta').innerHTML =
           'Continuando con los diseños aqui dejo uno nuevo, en este caso de una aplicacion de movil sobre una tienda de videojuegos, que tambien podria servir como base para una tienda en general. Espero que os guste.';
         tarjeta.querySelector('.fotos-tarjeta img').src =
-          '/assets/img/trabajos/random_x/randomx_presentation.png';
+          '../assets/img/trabajos/random_x/randomx_presentation.png';
         break;
       case "L'evasion":
         tarjeta.querySelector('#titulo-tarjeta').innerHTML = "L'evasion";
         tarjeta.querySelector('#texto-tarjeta').innerHTML =
           'Continuando con los diseños aqui dejo uno nuevo, en este caso de una aplicacion de movil sobre una tienda de videojuegos, que tambien podria servir como base para una tienda en general. Espero que os guste.';
         tarjeta.querySelector('.fotos-tarjeta img').src =
-          '/assets/img/trabajos/random_x/randomx_presentation.png';
+          '../assets/img/trabajos/random_x/randomx_presentation.png';
         break;
 
       default:
@@ -86,7 +86,7 @@
         tarjeta.querySelector('#texto-tarjeta').innerHTML =
           'Esta es una tarjeta por defecto';
         tarjeta.querySelector('.fotos-tarjeta img').src =
-          '/assets/img/trabajos/trabajo.png';
+          '../assets/img/trabajos/trabajo.png';
         break;
     }
   }
