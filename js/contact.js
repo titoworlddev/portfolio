ea7d75bc9d29b9ab5e7a71b182816dcca6f68a36
titoworlddev <titocristian.dev@gmail.com>
2022-09-15 22:55:18 +0200

    Trying to make js files with protected function to avoid the ads

--- js/contact.js
+++ js/contact.js
@@ -1,83 +1,108 @@
-  const btnSubmit = document.querySelector('.btn-submit')
-  const inputName = document.getElementById('name')
-  const inputEmail = document.getElementById('email')
-  const inputMessage = document.getElementById('message')
-  const errorName = document.querySelector('.error-name')
-  const errorEmail = document.querySelector('.error-email')
-  const errorMessage = document.querySelector('.error-message')
+(function () {
+  const btnSubmit = document.querySelector('.btn-submit');
+  const inputName = document.getElementById('name');
+  const inputEmail = document.getElementById('email');
+  const inputMessage = document.getElementById('message');
+  const errorName = document.querySelector('.error-name');
+  const errorEmail = document.querySelector('.error-email');
+  const errorMessage = document.querySelector('.error-message');
 
-  const emailExpr = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+  const emailExpr = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 
-  const inputs = [inputName, inputEmail, inputMessage]
-  inputs.forEach(input => {
+  const inputs = [inputName, inputEmail, inputMessage];
+  inputs.forEach((input) => {
     input.addEventListener('keyup', () => {
       if (input === inputName && errorName.style.display === 'block') {
-        errorName.style.display = 'none'
-        inputName.style.border = 'none'
+        errorName.style.display = 'none';
+        inputName.style.border = 'none';
       }
       if (input === inputEmail && errorEmail.style.display === 'block') {
-        errorEmail.style.display = 'none'
-        inputEmail.style.border = 'none'
+        errorEmail.style.display = 'none';
+        inputEmail.style.border = 'none';
       }
       if (input === inputMessage && errorMessage.style.display === 'block') {
-        errorMessage.style.display = 'none'
-        inputMessage.style.border = 'none'
+        errorMessage.style.display = 'none';
+        inputMessage.style.border = 'none';
       }
-    })
-  })
+    });
+  });
 
   showEmailError = (text) => {
-    errorEmail.innerHTML = text
-    errorEmail.style.display = 'block'
-    inputEmail.style.border = '0.6mm solid #e23838'
-  }
+    errorEmail.innerHTML = text;
+    errorEmail.style.display = 'block';
+    inputEmail.style.border = '0.6mm solid #e23838';
+  };
   showNameError = (text) => {
-    errorName.innerHTML = text
-    errorName.style.display = 'block'
-    inputName.style.border = '0.6mm solid #e23838'
-  }
+    errorName.innerHTML = text;
+    errorName.style.display = 'block';
+    inputName.style.border = '0.6mm solid #e23838';
+  };
 
   btnSubmit.addEventListener('mouseup', () => {
-    if (inputName.value === '' || inputEmail.value === '' || inputMessage.value === '') {
+    if (
+      inputName.value === '' ||
+      inputEmail.value === '' ||
+      inputMessage.value === ''
+    ) {
       if (inputName.value === '') {
-        showNameError('You must put the name so that I can address you in my answer.')
+        showNameError(
+          'You must put the name so that I can address you in my answer.'
+        );
       } else if (inputName.value.length < 3) {
-        showNameError('The name is not valid, it must contain a minimum of 3 letters.')
+        showNameError(
+          'The name is not valid, it must contain a minimum of 3 letters.'
+        );
       }
       if (inputEmail.value === '') {
-        showEmailError('You must put the email so that I can answer you.')
+        showEmailError('You must put the email so that I can answer you.');
       } else if (!emailExpr.test(inputEmail.value)) {
-        showEmailError('The email is not valid, you must enter a correct email to continue.')
+        showEmailError(
+          'The email is not valid, you must enter a correct email to continue.'
+        );
       } else if (inputEmail.value.includes('reply')) {
-        showEmailError('Please, do not use the contact form to send advertising, its use is exclusive for hiring.')
+        showEmailError(
+          'Please, do not use the contact form to send advertising, its use is exclusive for hiring.'
+        );
       }
       if (inputMessage.value === '') {
-        errorMessage.style.display = 'block'
-        inputMessage.style.border = '0.6mm solid #e23838'
+        errorMessage.style.display = 'block';
+        inputMessage.style.border = '0.6mm solid #e23838';
       }
     } else {
-      if (!emailExpr.test(inputEmail.value) || inputEmail.value.includes('reply') || inputName.value.length < 3) {
+      if (
+        !emailExpr.test(inputEmail.value) ||
+        inputEmail.value.includes('reply') ||
+        inputName.value.length < 3
+      ) {
         if (!emailExpr.test(inputEmail.value)) {
-          showEmailError('The email is not valid, you must enter a correct email to continue.')
+          showEmailError(
+            'The email is not valid, you must enter a correct email to continue.'
+          );
         }
         if (inputEmail.value.includes('reply')) {
-          showEmailError('Please, do not use the contact form to send advertising, its use is exclusive for hiring.')
+          showEmailError(
+            'Please, do not use the contact form to send advertising, its use is exclusive for hiring.'
+          );
         }
         if (inputName.value.length < 3) {
-          showNameError('The name is not valid, it must contain a minimum of 3 letters.')
+          showNameError(
+            'The name is not valid, it must contain a minimum of 3 letters.'
+          );
         }
       } else {
-          const contactContainer = document.querySelector('.contact-container')
-          const contactForm = document.getElementById('contact-form')
-          
-          const popup = document.createElement('div')
-          popup.classList.add('contact-popup')
-          popup.innerHTML = '<p>Your message has been sent successfully</p><i class="fa-solid fa-check"></i>'
-          contactContainer.appendChild(popup)
-          
-          setTimeout(function () {
-            contactForm.submit()
-          }, 200)
+        const contactContainer = document.querySelector('.contact-container');
+        const contactForm = document.getElementById('contact-form');
+
+        const popup = document.createElement('div');
+        popup.classList.add('contact-popup');
+        popup.innerHTML =
+          '<p>Your message has been sent successfully</p><i class="fa-solid fa-check"></i>';
+        contactContainer.appendChild(popup);
+
+        setTimeout(function () {
+          contactForm.submit();
+        }, 200);
       }
     }
-  })
+  });
+})();
--- js/projects.js
+++ js/projects.js
@@ -1,152 +1,170 @@
-/* ==================== Tarjeta ==================== */
-const tarjeta = document.getElementById('tarjeta')
-const tarjetaCloseBtn = document.getElementById('tarjeta-close-btn')
+(function () {
+  /* ==================== Tarjeta ==================== */
+  const tarjeta = document.getElementById('tarjeta');
+  const tarjetaCloseBtn = document.getElementById('tarjeta-close-btn');
 
-tarjetaCloseBtn.addEventListener('click', () => {
-  tarjeta.style.display = 'none'
-})
+  tarjetaCloseBtn.addEventListener('click', () => {
+    tarjeta.style.display = 'none';
+  });
 
-const tarjetaAssets = {
-  Programacion: {
-    'Random X': {
-      title: 'Random X App',
-      text: 'Random X is your assistant when it comes to playing and/or having fun with friends, its random generators give you options to randomly choose who pays for dinner, make the invisible friend with friends or family, make a bingo, play on your own Scattergories or whatever comes to mind. Undoubtedly the perfect companion in these cases, do not wait any longer and let the fun begin.',
-      img: '../assets/img/trabajos/random_x/randomx-presentation.png',
-      coverImg: '../assets/img/trabajos/random-x.png',
-      linkImg: '../assets/img/trabajos/store-badges/google-play-badge.svg',
-      url: 'https://play.google.com/store/apps/details?id=com.hayaxelectronics.random_x_app&hl=es'
+  const tarjetaAssets = {
+    Programacion: {
+      'Random X': {
+        title: 'Random X App',
+        text: 'Random X is your assistant when it comes to playing and/or having fun with friends, its random generators give you options to randomly choose who pays for dinner, make the invisible friend with friends or family, make a bingo, play on your own Scattergories or whatever comes to mind. Undoubtedly the perfect companion in these cases, do not wait any longer and let the fun begin.',
+        img: '../assets/img/trabajos/random_x/randomx-presentation.png',
+        coverImg: '../assets/img/trabajos/random-x.png',
+        linkImg: '../assets/img/trabajos/store-badges/google-play-badge.svg',
+        url: 'https://play.google.com/store/apps/details?id=com.hayaxelectronics.random_x_app&hl=es'
+      },
+      Operit: {
+        title: 'Operit App',
+        text: "Calculator with a simple design but very complete in its functions. If you are looking to do quick operations you have its basic part, if on the contrary you are a more advanced user you can use the scientific calculator. There are also several settings to customize the app to your liking and a page help if you don't understand any of the functions. Definitely a calculator that will be easy and fun to use, enjoy!",
+        img: '../assets/img/trabajos/operit/operit-presentation.png',
+        coverImg: '../assets/img/trabajos/operit.png',
+        linkImg: '../assets/img/trabajos/store-badges/google-play-badge.svg',
+        url: 'https://play.google.com/store/apps/details?id=com.hayaxelectronics.operit&gl=ES'
+      }
     },
-    Operit: {
-      title: 'Operit App',
-      text: 'Calculator with a simple design but very complete in its functions. If you are looking to do quick operations you have its basic part, if on the contrary you are a more advanced user you can use the scientific calculator. There are also several settings to customize the app to your liking and a page help if you don\'t understand any of the functions. Definitely a calculator that will be easy and fun to use, enjoy!',
-      img: '../assets/img/trabajos/operit/operit-presentation.png',
-      coverImg: '../assets/img/trabajos/operit.png',
-      linkImg: '../assets/img/trabajos/store-badges/google-play-badge.svg',
-      url: 'https://play.google.com/store/apps/details?id=com.hayaxelectronics.operit&gl=ES'
+    Diseno: {
+      'Game Zone': {
+        title: 'Game Zone Store - Design',
+        text: 'Continuing with the designs here I leave a new one, in this case of a mobile application on a video game store, which could also serve as the basis for a store in general. I hope you like it. <br/><br/>Enjoy all my designs on my instagram.',
+        img: '../assets/img/trabajos/game_zone/game-zone-presentation.png',
+        coverImg: '../assets/img/trabajos/game-zone-store.png',
+        linkImg: '../assets/img/trabajos/store-badges/instagram-logo.svg',
+        url: 'https://www.instagram.com/cristian_am91/'
+      },
+      'El Chingón': {
+        title: 'El Chingón Restaurant - Design',
+        text: 'Design about a Mexican food restaurant, which can be taken as an example of designs for restaurants. A design is very simple, but very attractive. I hope you like it. <br/><br/>Enjoy all my designs on my instagram.',
+        img: '../assets/img/trabajos/el_chingon/el-chingon-presentation.png',
+        coverImg: '../assets/img/trabajos/el-chingon.png',
+        linkImg: '../assets/img/trabajos/store-badges/instagram-logo.svg',
+        url: 'https://www.instagram.com/cristian_am91/'
+      },
+      "L'evasion": {
+        title: "L'evasion Hotel - Design",
+        text: 'Design on a hotel web page. A soft and relaxed design, which allows you to find a very pleasant hosting experience. I hope you like it. <br/><br/>Enjoy all my designs on my instagram.',
+        img: '../assets/img/trabajos/levasion/levasion-presentation.png',
+        coverImg: '../assets/img/trabajos/levasion.png',
+        linkImg: '../assets/img/trabajos/store-badges/instagram-logo.svg',
+        url: 'https://www.instagram.com/cristian_am91/'
+      }
     }
-  },
-  Diseno: {
-    'Game Zone': {
-      title: 'Game Zone Store - Design',
-      text: 'Continuing with the designs here I leave a new one, in this case of a mobile application on a video game store, which could also serve as the basis for a store in general. I hope you like it. <br/><br/>Enjoy all my designs on my instagram.',
-      img: '../assets/img/trabajos/game_zone/game-zone-presentation.png',
-      coverImg: '../assets/img/trabajos/game-zone-store.png',
-      linkImg: '../assets/img/trabajos/store-badges/instagram-logo.svg',
-      url: 'https://www.instagram.com/cristian_am91/'
-    },
-    'El Chingón': {
-      title: 'El Chingón Restaurant - Design',
-      text: 'Design about a Mexican food restaurant, which can be taken as an example of designs for restaurants. A design is very simple, but very attractive. I hope you like it. <br/><br/>Enjoy all my designs on my instagram.',
-      img: '../assets/img/trabajos/el_chingon/el-chingon-presentation.png',
-      coverImg: '../assets/img/trabajos/el-chingon.png',
-      linkImg: '../assets/img/trabajos/store-badges/instagram-logo.svg',
-      url: 'https://www.instagram.com/cristian_am91/'
-    },
-    "L'evasion": {
-      title: "L'evasion Hotel - Design",
-      text: 'Design on a hotel web page. A soft and relaxed design, which allows you to find a very pleasant hosting experience. I hope you like it. <br/><br/>Enjoy all my designs on my instagram.',
-      img: '../assets/img/trabajos/levasion/levasion-presentation.png',
-      coverImg: '../assets/img/trabajos/levasion.png',
-      linkImg: '../assets/img/trabajos/store-badges/instagram-logo.svg',
-      url: 'https://www.instagram.com/cristian_am91/'
+  };
+
+  function getTarjetaInfo(item) {
+    if (
+      Object.keys(tarjetaAssets.Programacion).includes(
+        item.querySelector('p').innerText
+      )
+    ) {
+      tarjeta.querySelector('#titulo-tarjeta').innerHTML =
+        tarjetaAssets.Programacion[item.querySelector('p').innerText].title;
+      tarjeta.querySelector('#texto-tarjeta').innerHTML =
+        tarjetaAssets.Programacion[item.querySelector('p').innerText].text;
+      tarjeta.querySelector('.fotos-tarjeta img').src =
+        tarjetaAssets.Programacion[item.querySelector('p').innerText].img;
+      tarjeta.querySelector('.logos-tiendas img').src =
+        tarjetaAssets.Programacion[item.querySelector('p').innerText].linkImg;
+      tarjeta
+        .querySelector('.logos-tiendas a')
+        .setAttribute(
+          'href',
+          tarjetaAssets.Programacion[item.querySelector('p').innerText].url
+        );
+    } else {
+      tarjeta.querySelector('#titulo-tarjeta').innerHTML =
+        tarjetaAssets.Diseno[item.querySelector('p').innerText].title;
+      tarjeta.querySelector('#texto-tarjeta').innerHTML =
+        tarjetaAssets.Diseno[item.querySelector('p').innerText].text;
+      tarjeta.querySelector('.fotos-tarjeta img').src =
+        tarjetaAssets.Diseno[item.querySelector('p').innerText].img;
+      tarjeta.querySelector('.logos-tiendas img').src =
+        tarjetaAssets.Diseno[item.querySelector('p').innerText].linkImg;
+      tarjeta
+        .querySelector('.logos-tiendas a')
+        .setAttribute(
+          'href',
+          tarjetaAssets.Diseno[item.querySelector('p').innerText].url
+        );
     }
   }
-}
 
-function getTarjetaInfo (item) {
-  if (Object.keys(tarjetaAssets.Programacion).includes(item.querySelector('p').innerText)) {
-    tarjeta.querySelector('#titulo-tarjeta').innerHTML =
-      tarjetaAssets.Programacion[item.querySelector('p').innerText].title
-    tarjeta.querySelector('#texto-tarjeta').innerHTML =
-      tarjetaAssets.Programacion[item.querySelector('p').innerText].text
-    tarjeta.querySelector('.fotos-tarjeta img').src =
-      tarjetaAssets.Programacion[item.querySelector('p').innerText].img
-    tarjeta.querySelector('.logos-tiendas img').src =
-      tarjetaAssets.Programacion[item.querySelector('p').innerText].linkImg
-    tarjeta.querySelector('.logos-tiendas a').setAttribute('href', tarjetaAssets.Programacion[item.querySelector('p').innerText].url)
-  } else {
-    tarjeta.querySelector('#titulo-tarjeta').innerHTML =
-      tarjetaAssets.Diseno[item.querySelector('p').innerText].title
-    tarjeta.querySelector('#texto-tarjeta').innerHTML =
-      tarjetaAssets.Diseno[item.querySelector('p').innerText].text
-    tarjeta.querySelector('.fotos-tarjeta img').src =
-      tarjetaAssets.Diseno[item.querySelector('p').innerText].img
-    tarjeta.querySelector('.logos-tiendas img').src =
-      tarjetaAssets.Diseno[item.querySelector('p').innerText].linkImg
-    tarjeta.querySelector('.logos-tiendas a').setAttribute('href', tarjetaAssets.Diseno[item.querySelector('p').innerText].url)
+  /* ==================== Trabajos ==================== */
+
+  /* Programacion */
+  Object.keys(tarjetaAssets.Programacion).forEach((title) => {
+    const programacionRow = document.querySelector(
+      '.trabajos-programacion-row'
+    );
+    const trabajo = document.createElement('div');
+    trabajo.classList.add('trabajo');
+    const blurMask = document.createElement('div');
+    blurMask.classList.add('blur-mask');
+    const img = document.createElement('img');
+    img.src = tarjetaAssets.Programacion[title].coverImg;
+    img.alt = title + ' Image';
+    const p = document.createElement('p');
+    p.innerHTML = title;
+    trabajo.appendChild(blurMask);
+    trabajo.appendChild(img);
+    trabajo.appendChild(p);
+    programacionRow.appendChild(trabajo);
+  });
+
+  const trabajosProgramacion = document.querySelectorAll(
+    '.trabajos-programacion-row .trabajo'
+  );
+
+  function displayMenuOrTarjet() {
+    const mobileMenu = document.getElementById('mobile-menu-items');
+    if (tarjeta.style.display !== 'block') {
+      tarjeta.style.display = 'block';
+    } else {
+      tarjeta.style.display = 'none';
+    }
+    if (mobileMenu.style.display !== 'block') {
+      mobileMenu.style.display = 'none';
+    } else {
+      mobileMenu.style.display = 'block';
+    }
   }
-}
 
-/* ==================== Trabajos ==================== */
+  trabajosProgramacion.forEach((item) => {
+    item.addEventListener('click', () => {
+      displayMenuOrTarjet();
+      getTarjetaInfo(item);
+    });
+  });
 
-/* Programacion */
-Object.keys(tarjetaAssets.Programacion).forEach(title => {
-  const programacionRow = document.querySelector('.trabajos-programacion-row')
-  const trabajo = document.createElement('div')
-  trabajo.classList.add('trabajo')
-  const blurMask = document.createElement('div')
-  blurMask.classList.add('blur-mask')
-  const img = document.createElement('img')
-  img.src = tarjetaAssets.Programacion[title].coverImg
-  img.alt = title + ' Image'
-  const p = document.createElement('p')
-  p.innerHTML = title
-  trabajo.appendChild(blurMask)
-  trabajo.appendChild(img)
-  trabajo.appendChild(p)
-  programacionRow.appendChild(trabajo)
-})
+  /* Diseno */
+  Object.keys(tarjetaAssets.Diseno).forEach((title) => {
+    const programacionRow = document.querySelector('.trabajos-diseno-row');
+    const trabajo = document.createElement('div');
+    trabajo.classList.add('trabajo');
+    const blurMask = document.createElement('div');
+    blurMask.classList.add('blur-mask');
+    const img = document.createElement('img');
+    img.src = tarjetaAssets.Diseno[title].coverImg;
+    img.alt = title + ' Image';
+    const p = document.createElement('p');
+    p.innerHTML = title;
+    trabajo.appendChild(blurMask);
+    trabajo.appendChild(img);
+    trabajo.appendChild(p);
+    programacionRow.appendChild(trabajo);
+  });
 
-const trabajosProgramacion = document.querySelectorAll(
-  '.trabajos-programacion-row .trabajo'
-)
+  const trabajosDiseno = document.querySelectorAll(
+    '.trabajos-diseno-row .trabajo'
+  );
 
-function displayMenuOrTarjet () {
-  const mobileMenu = document.getElementById('mobile-menu-items')
-  if (tarjeta.style.display !== 'block') {
-    tarjeta.style.display = 'block'
-  } else {
-    tarjeta.style.display = 'none'
-  }
-  if (mobileMenu.style.display !== 'block') {
-    mobileMenu.style.display = 'none'
-  } else {
-    mobileMenu.style.display = 'block'
-  }
-}
-
-trabajosProgramacion.forEach(item => {
-  item.addEventListener('click', () => {
-    displayMenuOrTarjet()
-    getTarjetaInfo(item)
-  })
-})
-
-/* Diseno */
-Object.keys(tarjetaAssets.Diseno).forEach(title => {
-  const programacionRow = document.querySelector('.trabajos-diseno-row')
-  const trabajo = document.createElement('div')
-  trabajo.classList.add('trabajo')
-  const blurMask = document.createElement('div')
-  blurMask.classList.add('blur-mask')
-  const img = document.createElement('img')
-  img.src = tarjetaAssets.Diseno[title].coverImg
-  img.alt = title + ' Image'
-  const p = document.createElement('p')
-  p.innerHTML = title
-  trabajo.appendChild(blurMask)
-  trabajo.appendChild(img)
-  trabajo.appendChild(p)
-  programacionRow.appendChild(trabajo)
-})
-
-const trabajosDiseno = document.querySelectorAll(
-  '.trabajos-diseno-row .trabajo'
-)
-
-trabajosDiseno.forEach(item => {
-  item.addEventListener('click', () => {
-    displayMenuOrTarjet()
-    getTarjetaInfo(item)
-  })
-})
+  trabajosDiseno.forEach((item) => {
+    item.addEventListener('click', () => {
+      displayMenuOrTarjet();
+      getTarjetaInfo(item);
+    });
+  });
+})();
