0a05b193011c8cbe5300faee09e45389b523ca16
titoworlddev <titocristian.dev@gmail.com>
2022-11-27 13:14:13 +0100

    Testing 8

--- js/contactTesting.js
+++ js/contactTesting.js
@@ -1,13 +1,11 @@
-(function () {
-  const $ = (selector) => document.querySelector(selector);
+const $ = (selector) => document.querySelector(selector);
 
-  const btnSubmit = $('.btn-submit');
-  const API_KEY = process.env.API_KEY;
-  const API_HOST = process.env.API_HOST;
-  const API_URL = process.env.API_URL;
+const btnSubmit = $('.btn-submit');
+const API_KEY = process.env.API_KEY;
+const API_HOST = process.env.API_HOST;
+const API_URL = process.env.API_URL;
 
-  btnSubmit.addEventListener('mouseup', async () => {
-    console.log('Submit button clicked');
-    console.log(API_KEY + ' ' + API_HOST + ' ' + API_URL);
-  });
-})();
+btnSubmit.addEventListener('mouseup', async () => {
+  console.log('Submit button clicked');
+  console.log(API_KEY + ' ' + API_HOST + ' ' + API_URL);
+});
