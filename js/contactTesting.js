c77549b80bb2766782b9b70933ed3e1d9fe7507c
titoworlddev <titocristian.dev@gmail.com>
2022-11-27 12:45:53 +0100

    Testing process

--- js/contactTesting.js
+++ js/contactTesting.js
@@ -7,6 +7,7 @@
   const API_URL = process.env.API_URL;
 
   btnSubmit.addEventListener('mouseup', async () => {
+    console.log('Submit button clicked');
     console.log(API_KEY + ' ' + API_HOST + ' ' + API_URL);
   });
 })();
