3cf4d6ad025ed09381a38a0bfe9550b0f96c78e4
titoworlddev <titocristian.dev@gmail.com>
2023-01-04 22:17:33 +0100

    Cambiados los formatos de imagen a webp, comprimidas algunas para mejorar el rendimiento. Cambiado el link de google fonts al final del body

--- js/projects.js
+++ js/projects.js
@@ -16,16 +16,16 @@ export const projects = () => {
         'Random X': {
           title: 'Random X App',
           text: 'Random X es tu asistente a la hora de jugar y/o divertirte con amigos, sus generadores aleatorios te dan opciones para elegir aleatoriamente quién paga la cena, hacer el amigo invisible con amigos o familiares, hacer un bingo, jugar a tu propio Scattergories o lo que se te ocurra. Sin duda el compañero perfecto en estos casos, no esperes más y que empiece la diversión.',
-          img: '/assets/img/trabajos/random_x/randomx-presentation.png',
-          coverImg: '/assets/img/trabajos/random-x.png',
+          img: '/assets/img/trabajos/random_x/randomx-presentation.webp',
+          coverImg: '/assets/img/trabajos/random-x.webp',
           linkImg: '/assets/img/trabajos/store-badges/google-play-badge.svg',
           url: 'https://play.google.com/store/apps/details?id=com.hayaxelectronics.random_x_app&hl=es'
         },
         Operit: {
           title: 'Operit App',
           text: 'Calculadora con un diseño sencillo pero muy completa en sus funciones. Si buscas hacer operaciones rápidas tienes su parte básica, si por el contrario eres un usuario más avanzado puedes utilizar la calculadora científica. También hay varias configuraciones para personalizar la aplicación a tu gusto y una página de ayuda si no entiendes alguna de las funciones. Definitivamente una calculadora que será fácil y divertida de usar, ¡disfrútala!',
-          img: '/assets/img/trabajos/operit/operit-presentation.png',
-          coverImg: '/assets/img/trabajos/operit.png',
+          img: '/assets/img/trabajos/operit/operit-presentation.webp',
+          coverImg: '/assets/img/trabajos/operit.webp',
           linkImg: '/assets/img/trabajos/store-badges/google-play-badge.svg',
           url: 'https://play.google.com/store/apps/details?id=com.hayaxelectronics.operit&gl=ES'
         }
@@ -35,24 +35,24 @@ export const projects = () => {
         'Game Zone': {
           title: 'Tienda Electrónica - Game Zone',
           text: 'Continuando con los diseños aquí les dejo uno nuevo, en este caso de una aplicación móvil sobre una tienda de videojuegos, que también podría servir como base para una tienda en general. Espero que te guste. <br/><br/>Disfruta de todos mis diseños en mi instagram.',
-          img: '/assets/img/trabajos/game_zone/game-zone-presentation.png',
-          coverImg: '/assets/img/trabajos/game-zone-store.png',
+          img: '/assets/img/trabajos/game_zone/game-zone-presentation.webp',
+          coverImg: '/assets/img/trabajos/game-zone-store.webp',
           linkImg: '/assets/img/trabajos/store-badges/instagram-logo.svg',
           url: 'https://www.instagram.com/cristian_am91/'
         },
         'El Chingón': {
           title: 'Restaurante - El Chingón',
           text: 'Diseño sobre un restaurante de comida mexicana, que se puede tomar como ejemplo de diseños para restaurantes. Es un diseño muy simple, pero a la vez muy atractivo. Espero que te guste. <br/><br/>Disfruta de todos mis diseños en mi instagram.',
-          img: '/assets/img/trabajos/el_chingon/el-chingon-presentation.png',
-          coverImg: '/assets/img/trabajos/el-chingon.png',
+          img: '/assets/img/trabajos/el_chingon/el-chingon-presentation.webp',
+          coverImg: '/assets/img/trabajos/el-chingon.webp',
           linkImg: '/assets/img/trabajos/store-badges/instagram-logo.svg',
           url: 'https://www.instagram.com/cristian_am91/'
         },
         "L'evasion": {
           title: "Hotel - L'evasion",
           text: 'Diseño en la página web de un hotel o apartamentos. Un diseño suave y relajado, que permite encontrar una experiencia de hospedaje muy placentera. Espero que te guste. <br/><br/>Disfruta de todos mis diseños en mi instagram.',
-          img: '/assets/img/trabajos/levasion/levasion-presentation.png',
-          coverImg: '/assets/img/trabajos/levasion.png',
+          img: '/assets/img/trabajos/levasion/levasion-presentation.webp',
+          coverImg: '/assets/img/trabajos/levasion.webp',
           linkImg: '/assets/img/trabajos/store-badges/instagram-logo.svg',
           url: 'https://www.instagram.com/cristian_am91/'
         }
@@ -90,7 +90,7 @@ export const projects = () => {
     /* ==================== Trabajos ==================== */
 
     /* Programacion */
-    Object.keys(tarjetaAssets.Programacion).forEach((title) => {
+    Object.keys(tarjetaAssets.Programacion).forEach(title => {
       const programacionRow = document.querySelector(
         '.trabajos-programacion-row'
       );
@@ -104,7 +104,7 @@ export const projects = () => {
     });
 
     /* Diseno */
-    Object.keys(tarjetaAssets.Diseno).forEach((title) => {
+    Object.keys(tarjetaAssets.Diseno).forEach(title => {
       const disenoRow = document.querySelector('.trabajos-diseno-row');
       const trabajo = `
       <div class="trabajo">
@@ -115,7 +115,7 @@ export const projects = () => {
       disenoRow.innerHTML += trabajo;
     });
 
-    document.querySelectorAll('.trabajo').forEach((item) => {
+    document.querySelectorAll('.trabajo').forEach(item => {
       item.addEventListener('click', () => {
         if (tarjetaContainer.style.display !== 'flex')
           tarjetaContainer.style.display = 'flex';
@@ -127,21 +127,21 @@ export const projects = () => {
 
 export const precacheImgs = () => {
   const images = [
-    '/assets/img/trabajos/random_x/randomx-presentation.png',
-    '/assets/img/trabajos/random-x.png',
+    '/assets/img/trabajos/random_x/randomx-presentation.webp',
+    '/assets/img/trabajos/random-x.webp',
     '/assets/img/trabajos/store-badges/google-play-badge.svg',
-    '/assets/img/trabajos/operit/operit-presentation.png',
-    '/assets/img/trabajos/operit.png',
-    '/assets/img/trabajos/game_zone/game-zone-presentation.png',
-    '/assets/img/trabajos/game-zone-store.png',
-    '/assets/img/trabajos/el_chingon/el-chingon-presentation.png',
-    '/assets/img/trabajos/el-chingon.png',
+    '/assets/img/trabajos/operit/operit-presentation.webp',
+    '/assets/img/trabajos/operit.webp',
+    '/assets/img/trabajos/game_zone/game-zone-presentation.webp',
+    '/assets/img/trabajos/game-zone-store.webp',
+    '/assets/img/trabajos/el_chingon/el-chingon-presentation.webp',
+    '/assets/img/trabajos/el-chingon.webp',
     '/assets/img/trabajos/store-badges/instagram-logo.svg',
-    '/assets/img/trabajos/levasion/levasion-presentation.png',
-    '/assets/img/trabajos/levasion.png'
+    '/assets/img/trabajos/levasion/levasion-presentation.webp',
+    '/assets/img/trabajos/levasion.webp'
   ];
   const cachedImages = [];
-  images.forEach((elem) => {
+  images.forEach(elem => {
     cachedImages.push(document.createElement('img'));
     cachedImages.at(-1).src = elem;
   });
