e79deb8f6cb81f08f4f2f150f47acc114acabe9e
titoworlddev <titocristian.dev@gmail.com>
2023-01-17 18:17:45 +0100

    Refactoring getJSON function

--- js/projects/tarjetasAssets.js
+++ js/projects/tarjetasAssets.js
@@ -1,10 +1,10 @@
 const getJSON = async url => {
   try {
-    return await (await fetch(url)).json();
+    return await fetch(url).then(res => res.json());
   } catch (e) {
     return {};
   }
 };
 
-// estoy exportando la data al hacer uso del await antes de getJSON, si no lo hago, no puedo usar la data en los otros archivos porque exporta una promesa y no la data en sí
+// estoy exportando la data al hacer uso del await antes de getJSON, si no lo hago, no puedo usar la data en los otros archivos porque exporta una promesa y no la data en sí ya que no se ha resuelto la promesa
 export const tarjetasAssets = await getJSON('/assets/json/tarjetaAssets.json');
