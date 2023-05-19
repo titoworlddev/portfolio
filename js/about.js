746f21436248fb5253f5dbb3ba349595d03e2b3c
Cristian Arias Mejuto <titocristian.dev@gmail.com>
2023-05-19 23:43:08 +0200

    Fixing aboutMe error

--- js/about.js
+++
@@ -1,7 +0,0 @@
-export function aboutMe() {
-  let currentDate = new Date();
-  let myBirthdate = new Date('10/20/1991');
-  let ageDate = new Date(currentDate - myBirthdate);
-  let myCurrentAge = document.querySelector('#my-current-age');
-  myCurrentAge.innerHTML = Math.abs(ageDate.getUTCFullYear() - 1970);
-}
