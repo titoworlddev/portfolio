4ce1e95924e612101e8bd327a9ce82134f405cf3
Cristian Arias Mejuto <titocristian.dev@gmail.com>
2023-05-19 23:43:05 +0200

    Fixing aboutMe error

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ import { closeMenuCard, btnUpPage } from '/js/generalFunctions.js';
 import { handleMenu } from '/js/menu.js';
 import { projects } from '/js/projects/projects.js';
 import { contact } from '/js/contact/contact.js';
-import { aboutMe } from '/js/about.js';
 import { setPathName } from '/js/setPathName.js';
 
 (function () {
@@ -13,8 +12,6 @@ import { setPathName } from '/js/setPathName.js';
   btnUpPage();
   /* Projects */
   projects();
-  /* About Me */
-  aboutMe();
   /* Contact */
   contact();
 })();
