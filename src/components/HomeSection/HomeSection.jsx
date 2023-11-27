89ddf82b1e9ca4ac4f1060440a8354f0475ac932
Cristian Arias Mejuto <titocristian.dev@gmail.com>
2023-11-27 21:50:22 +0100

    feat: Added short description at intro

--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -2,17 +2,24 @@ import './_HomeSection.scss';
 
 export default function HomeSection() {
   return (
-    <section className='home'>
-      <div className='home-texts'>
+    <section className="home">
+      <div className="home-texts">
         <p>Cristian Arias</p>
-        <h1 className='title-text'>Desarrollador FrontEnd</h1>
+        <h1 className="title-text">Desarrollador FrontEnd</h1>
+        <small className="description">
+          Apasionado desarrollador frontend con +3 años de experiencia en el
+          estudio de desarrollo web y móvil. <br /> Busco oportunidades para
+          aplicar y ampliar mis conocimientos en el desarrollo frontend,
+          aportando entusiasmo y un enfoque centrado en la usabilidad y el
+          diseño.
+        </small>
       </div>
 
-      <div className='home-img'>
+      <div className="home-img">
         <img
-          className='foto'
-          src='/assets/img/mis-fotos/mi-foto.webp'
-          alt='My Photo'
+          className="foto"
+          src="/assets/img/mis-fotos/mi-foto.webp"
+          alt="My Photo"
         />
       </div>
     </section>
