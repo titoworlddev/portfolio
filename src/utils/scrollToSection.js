c7e0ce14ed9787c133d1a652d21ed947888dd661
Cristian Arias Mejuto <titocristian.dev@gmail.com>
2023-12-07 23:25:42 +0100

    refactor: Changed scrollToSection

--- src/utils/scrollToSection.js
+++ src/utils/scrollToSection.js
@@ -1,18 +1,19 @@
-export function scrollToSection(ref) {
+export function scrollToSection() {
   const path = window.location.pathname;
+  const simpleBarContent = document.querySelector(
+    '.my-simplebar .simplebar-content-wrapper'
+  );
   const scroll = path => {
-    ref.getScrollElement().scroll({
+    simpleBarContent.scroll({
       left: 0,
       top: document.querySelector(`.${path}`).offsetTop - 96,
       behavior: 'instant'
     });
   };
 
-  if (path === '/projects') {
-    scroll('projects');
-  } else if (path === '/about') {
-    scroll('about');
-  } else if (path === '/contact') {
-    scroll('contact');
+  const sections = ['/projects', '/about', '/contact'];
+
+  if (sections.includes(path)) {
+    scroll(path.slice(1));
   }
 }
