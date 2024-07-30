bcac91f931176b34d38bef6133b1a7e585381892
titoworlddev <titocristian.dev@gmail.com>
2024-07-30 21:57:21 +0200

    Creado loader y mejorada la carga de los proyectos

--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -1,24 +1,25 @@
 import { useState } from 'react';
-import ProjectCard from '../ProjectCard/ProjectCard';
 import './_ProjectsSection.scss';
 import { getJSON } from '/src/utils/projects/tarjetasAssets';
 import Project from '../Project/Project';
 import { useEffect } from 'react';
+import Loader from '../Loader/Loader';
 
 export default function ProjectsSection() {
   const [category, setCategory] = useState('web');
-  const [project, setProject] = useState({});
   const [tarjetasAssets, setTarjetasAssets] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    getJSON('/assets/json/tarjetaAssets.json').then(assets =>
-      setTarjetasAssets(assets)
-    );
+    setIsLoading(true);
+    getJSON('/assets/json/tarjetaAssets.json').then(assets => {
+      setTarjetasAssets(assets);
+      setTimeout(() => setIsLoading(false), 500);
+    });
   }, []);
 
-  const categoryEntries = Object.entries(tarjetasAssets).filter(
-    ([, v]) => v.category === category
-  );
+  const categoryEntries = e =>
+    Object.entries(tarjetasAssets).filter(([, v]) => v.category === e);
 
   const handleSetCategory = e => {
     const category = e.target.id;
@@ -50,8 +51,24 @@ export default function ProjectsSection() {
         </li>
       </ul>
 
-      <div id={`trabajos-${category}`} className="trabajos-container">
-        {categoryEntries.map(([key, value]) => (
+      {isLoading && <Loader />}
+
+      <div
+        className="trabajos-container"
+        style={{ display: !isLoading && category === 'web' ? 'grid' : 'none' }}
+      >
+        {categoryEntries('web').map(([key, value]) => (
+          <Project key={key} name={key} project={value} />
+        ))}
+      </div>
+
+      <div
+        className="trabajos-container"
+        style={{
+          display: !isLoading && category === 'movil' ? 'grid' : 'none'
+        }}
+      >
+        {categoryEntries('movil').map(([key, value]) => (
           <Project key={key} name={key} project={value} />
         ))}
       </div>
